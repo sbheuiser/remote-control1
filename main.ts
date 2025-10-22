31007601ba49b92755b91efd75f8eafd50375216
sbheuiser <sbheuiser@gmail.com>
2025-10-22 14:11:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,46 +1,26 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
-    basic.showLeds(`
-        . # # # .
-        . # . . .
-        . # # # .
-        . # . . .
-        . # # # .
-        `)
+    basic.showString("E")
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
+    basic.showString("A")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
-    basic.showLeds(`
-        . # # # .
-        . # . . .
-        . # # # .
-        . # . . .
-        . # . . .
-        `)
+    basic.showString("F")
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(3)
+    basic.showString("AB")
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
+    basic.showString("B")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
-    basic.showLeds(`
-        . # # . .
-        . # . # .
-        . # . # .
-        . # . # .
-        . # # . .
-        `)
+    basic.showString("D")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
-    basic.showLeds(`
-        . # # # .
-        . # . . .
-        . # . . .
-        . # . . .
-        . # # # .
-        `)
+    basic.showString("C")
 })
 radio.setGroup(1)
+basic.showIcon(IconNames.Chessboard)
