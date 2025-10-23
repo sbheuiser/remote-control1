c9c8b3d86922b3a41fb52d6f23f119c9de0ee996
sbheuiser <sbheuiser@gmail.com>
2025-10-22 22:47:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,33 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
     joystickbit.Vibration_Motor(100)
-    basic.showString("E")
+    basic.showString("S")
     radio.sendNumber(4)
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
-    basic.showString("A")
+    basic.showString("L")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
     joystickbit.Vibration_Motor(100)
-    basic.showString("F")
+    basic.showString("S")
     radio.sendNumber(4)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(3)
-    basic.showString("AB")
+    basic.showString("F")
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
-    basic.showString("B")
+    basic.showString("R")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     joystickbit.Vibration_Motor(100)
-    basic.showString("D")
+    basic.showString("S")
     radio.sendNumber(4)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     joystickbit.Vibration_Motor(100)
-    basic.showString("C")
+    basic.showString("S")
     radio.sendNumber(4)
 })
 radio.setGroup(1)
